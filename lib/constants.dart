d92e4ebeccbe4fd889d8581fc69bc2ac090625d8
krrajpurohit <kr890794@gmail.com>
2020-01-26 16:08:19 +0530

    custom theme applied on slider

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 const kActiveCardColor = Color(0xFF1D1E33);
 const kInactiveCardColor = Color(0xFF1D1E3);
 const kBottomContainerColor = Color(0xFFEB1555);
+const kSliderOverlayColor = Color(0x29EB1555);
+const kSliderThumbColor = Color(0xFFEB1555);
 const kBottomContainerHeight = 80.0;
 const kSliderMin = 120.0;
 const kSliderMax = 220.0;
